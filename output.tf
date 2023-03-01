bafd6fea5094cd1cd9ca0ae9d44a66d6e988a0f9
ayushme001 <ayush.me001@gmail.com>
2023-03-01 14:24:03 +0530

    added kong changes

--- output.tf
+++ output.tf
@@ -1,7 +1,7 @@
-output "product_vpc" {
-  value = data.aws_eks_cluster.example
-}
+# output "product_vpc" {
+#   value = data.aws_eks_cluster.example
+# }
 
-output "data_arn" {
-  value = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:oidc-provider/${trimprefix(data.aws_eks_cluster.example.identity[0].oidc[0].issuer, "https://")}"
-}
+# output "data_arn" {
+#   value = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:oidc-provider/${trimprefix(data.aws_eks_cluster.example.identity[0].oidc[0].issuer, "https://")}"
+# }
